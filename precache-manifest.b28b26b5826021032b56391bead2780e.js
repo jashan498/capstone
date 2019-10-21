c7f494571bde9556936b0d70b3d9c08ca176926c
jashan498 <jashansingh.4398@gmail.com>
2019-10-21 17:17:24 +0530

    Updates

---
+++ precache-manifest.b28b26b5826021032b56391bead2780e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64b0c223d3f18225ab3a",
+    "url": "/capstone/static/js/runtime~main.64b0c223.js"
+  },
+  {
+    "revision": "588a32e0c5b3f07b25ae",
+    "url": "/capstone/static/js/main.588a32e0.chunk.js"
+  },
+  {
+    "revision": "10d7bd99756cc57e2ae4",
+    "url": "/capstone/static/js/1.10d7bd99.chunk.js"
+  },
+  {
+    "revision": "588a32e0c5b3f07b25ae",
+    "url": "/capstone/static/css/main.1e36e236.chunk.css"
+  },
+  {
+    "revision": "10d7bd99756cc57e2ae4",
+    "url": "/capstone/static/css/1.d40d2989.chunk.css"
+  },
+  {
+    "revision": "ee93b6b9a24a63d8c05de91703ae2912",
+    "url": "/capstone/index.html"
+  }
+];
